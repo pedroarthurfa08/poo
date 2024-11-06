fcc7acbd7c3556093156204109441ffff3921b7e
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2024-11-06 20:51:36 -0300

    comit

--- exercicios/Veiculo.py
+++ exercicios/Veiculo.py
@@ -1,17 +1,15 @@
 class Veiculo:
     def __init__(self, placa, marca, modelo, ano, cor = "não especifica", proprietário = "não especifica", quilometragem = 0):
-        # Atributos obrigatório
         self.placa = placa
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
-        # Atributos não obrigatórios
+
         self.cor = cor
         self.proprietário = proprietário
         self.quilometragem = quilometragem
     
-    def exibir_infomacoes(self):
-        # Mostra informações do veículo
+    def exibir_informacoes(self):
         print(f'Placa: {self.placa}')
         print(f'Marca: {self.marca}')
         print(f'Modelo: {self.modelo}')
@@ -20,19 +18,27 @@ class Veiculo:
         print(f'Proprietário: {self.proprietário}')
         print(f'Quilometragem: {self.quilometragem}')
 
-    def atualiar_quilometragem(self, nova_quilometragem):
-        # Atualiza a quilometragem do veículo
-        self.nova_quilometragem = nova_quilometragem
+    def atualizar_quilometragem(self, nova_quilometragem):
+        if nova_quilometragem >= self.quilometragem:
+            self.quilometragem = nova_quilometragem
+            print("Quilometragem atualziada.")
+        else:
+            return "Erro, tente novamente mais tarde, obrigado."
+
+placa = input("Digite a placa do veículo: ")
+marca = input("Digite a marca do veículo: ")
+modelo = input("Digite o modelo do veículo: ")
+ano = int(input("Digite o ano de fabricação do veículo: "))
+cor = input("Digite a cor do veículo (ou deixe em branco para 'Não especificada'): ") or "Não especificada"
+proprietario = input("Digite o nome do proprietário (ou deixe em branco para 'Não especificado'): ") or "Não especificado"
+quilometragem = int(input("Digite a quilometragem atual (ou deixe em branco para 0): ") or 0)
 
 def main():
-    cadastro = Veiculo()
-    while True:
-        placa = input(f'Insira placa do veículo: ')
-        marca = input(f'Insira marca do veículo: ')
-        modelo = input(f'Insira modelo do veículo: ')
-        ano = int(input(f'Insira ano do veículo: '))
-        cor = input(f'Insira cor do veículo (ou pressione ENTER para não especificar): ')
-        proprietario = input(f'Insira proprietário do veículo (ou pressione ENTER para não especificar): ')
-        quilometragem = int(input(f'Insira quilometragem do veículo (ou pressione ENTER para não especificar): '))
-        cadastro = Veiculo(placa, marca, modelo, ano, cor, proprietario, quilometragem)
+    veiculo = Veiculo(placa, marca, modelo, ano, cor, proprietario, quilometragem)
+
+    veiculo.exibir_informacoes()
+
+    nova_quilometragem = int(input("Digite a nova quilometragem para atualizar: "))
+    veiculo.atualizar_quilometragem(nova_quilometragem)
+    veiculo.exibir_informacoes()
 main()
